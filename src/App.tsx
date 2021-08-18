b0989a3fe733b56fdf236bd164168493b612bc29
Ratul <ratulkhan1428@gmail.com>
2021-08-18 22:12:27 +0600

    fvaly project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import AppRouter from './AppRouter';
+import Header from './components/common/Header';
+
 function App() {
   return (
-    <div>
-      <h1>Hello Fvaly!</h1>
-    </div>
+    <>
+      <AppRouter>
+        <Header />
+      </AppRouter>
+    </>
   );
 }
 
